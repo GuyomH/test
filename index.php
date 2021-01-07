8eea226803b5cf4f8be465cbe74d1132b71a1a31
GuyomH <guyomh@gmail.com>
2021-01-07 18:38:49 +0100

    Adding new multiply function

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <h1>Tests sur les branches et les tags</h1>
 
 <?php
+require_once 'inc/multiply.inc.php';
+
 function add(int $a, int $b): ?int
 {
     return $a + $b;
@@ -15,3 +17,9 @@ $c = 8;
 $d = 2;
 
 echo "<p>" . $c . " + " . $d . " = " . add($c, $d) . "</p>";
+
+// Multiplication
+$e = 2;
+$f = 12;
+
+echo "<p>" . $c . " * " . $d . " = " . multiply($e, $f) . "</p>";
